6df2785a4316352c18ce8c0056dd0d65b37ad421
vinay <vinaydahiya04@gmail.com>
2021-01-30 10:36:41 +0530

    initialized repo

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoSantize = require("express-mongo-sanitize")
+const PORT = process.env.PORT || 4000;
+//ROUTES
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set("port", PORT);
+app.use(cors());
+app.use(mongoSantize())
+
+var server = app.listen(PORT, () => {
+  console.log("server listening at " + PORT);
+});
+//
+
+
+
+
+
+//working check
+app.get("/", async (req, res) => {
+  return res.json({ message: "working" });
+});
+
+//Mail.signup()
