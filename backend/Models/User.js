3acf6ae4287755b675a3a18468ddf897fcebf278
vinay <vinaydahiya04@gmail.com>
2021-01-30 10:53:40 +0530

    added User model

---
+++ backend/Models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name']
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ],
+    },
+    phone: {
+        type: Number
+    },
+    password: {
+        type: String
+    },
+    googleId: {
+        type: String
+    },
+    facebookId: {
+        type: String
+    },
+    vehicles: {
+        type: [
+            {
+                vehicle: { type: Schema.Types.ObjectId, ref: "Vehicle" }
+
+            }
+        ],
+    },
+
+
+})
+
+module.exports = mongoose.model('User', UserSchema);
